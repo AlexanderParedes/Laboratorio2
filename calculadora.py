98848feda88429648b47fb3daf929ed8ec93ed73
AlexanderParedes <ceoalx@gmail.com>
2021-05-28 15:58:03 -0500

    Create calculadora.py
    Se añade la operación resta

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+class Calculadora:
+  #Restricción de valores negativos
+  #Operación Suma
+      def suma(self, val1, val2):
+        if (val1 < 0 or val2 < 0):
+            raise ArithmeticError("No se aceptan valores negativos")
+        return val1 + val2
+      
+ 
+  
