b6b46311364fbaaf61f2d2458cb0a65dd1a7a15f
AlexanderParedes <ceoalx@gmail.com>
2021-05-28 16:34:46 -0500

    Update calculadora.py
    se ha añadido la operación Raiz Cuadrada

--- calculadora.py
+++ calculadora.py
@@ -30,3 +30,18 @@ class Calculadora:
             cociente = self.suma(cociente, 1)
             residue = self.resta(residue, d)
         return (cociente, residue)
+
+  #Operación Raiz Cuadrada
+    def raiz(self, val):
+        a = self.multiplicar(5, val)
+        b = 5
+        while b < 10000000:
+            if (a >= b):
+                a = self.resta(a,b)
+                b = self.suma(b, 10)
+            else:
+                a = self.multiplicar(a, 100)
+                b = self.multiplicar(b, 10)
+                b = self.resta(b, 45)
+        b = self.resta(b, 5)
+        return self.dividir(b, 10000000)
