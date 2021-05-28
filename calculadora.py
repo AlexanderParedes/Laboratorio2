a452413bff67b36f1f1dbf5ee80194b76f1a6e53
AlexanderParedes <ceoalx@gmail.com>
2021-05-28 16:23:46 -0500

    Update calculadora.py
    se ha añadido la operación multiplicar

--- calculadora.py
+++ calculadora.py
@@ -6,9 +6,17 @@ class Calculadora:
             raise ArithmeticError("No se aceptan valores negativos")
         return val1 + val2
       
- #Operación Resta
+  #Operación Resta
     def resta(self, val1, val2):
         if (val1 < 0 or val2 < 0):
             raise ArithmeticError("No se aceptan valores negativos")
         return val1 - val2
   
+  #Operación Multiplicar
+    def multiplicar(self, val1, val2):
+        if (val1 < 0 or val2 < 0):
+            raise ArithmeticError("No se aceptan valores negativos")
+        resultado = 0
+        for i in range(val2):
+            resultado = self.suma(resultado,val1)
+        return resultado
