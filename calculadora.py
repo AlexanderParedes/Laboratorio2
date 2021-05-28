5a48dfd0cb3a97514fc35664742d375fc3812d9a
AlexanderParedes <ceoalx@gmail.com>
2021-05-28 16:09:26 -0500

    Update calculadora.py
    Se a añadido la operación resta

--- calculadora.py
+++ calculadora.py
@@ -6,5 +6,9 @@ class Calculadora:
             raise ArithmeticError("No se aceptan valores negativos")
         return val1 + val2
       
- 
+ #Operación Resta
+    def resta(self, val1, val2):
+        if (val1 < 0 or val2 < 0):
+            raise ArithmeticError("No se aceptan valores negativos")
+        return val1 - val2
   
